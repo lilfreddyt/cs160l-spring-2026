e902e243ccba51aef43e84429b4d09822af6a9ac
Yahui Jiang <yahui.jiang.rgb@gmail.com>
2026-03-03 18:27:26 -0800

    lab7

--- Lab7/src/main/java/edu/sdsu/cs160l/university/lab7/exceptions/NoSuchCourseException.java
+++ Lab7/src/main/java/edu/sdsu/cs160l/university/lab7/exceptions/NoSuchCourseException.java
@@ -2,6 +2,5 @@ package edu.sdsu.cs160l.university.lab7.exceptions;
 
 import java.util.NoSuchElementException;
 
-//TODO implement this exception as per the bluePrint in ClassFullException
 public class NoSuchCourseException extends NoSuchElementException {
 }
--- Lab7/src/main/java/edu/sdsu/cs160l/university/lab7/exceptions/StudentAlreadyEnrolledException.java
+++ Lab7/src/main/java/edu/sdsu/cs160l/university/lab7/exceptions/StudentAlreadyEnrolledException.java
@@ -1,5 +1,4 @@
 package edu.sdsu.cs160l.university.lab7.exceptions;
 
-//TODO implement this exception as per the bluePrint in ClassFullException
 public class StudentAlreadyEnrolledException extends Exception{
 }
